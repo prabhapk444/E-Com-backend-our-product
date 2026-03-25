72f64b591f24e273bd218d21208240106b1aa16c
prabhapk444 <karanprabha22668@gmail.com>
2026-03-25 08:25:17 +0530

    base url change

--- application/config/config.php
+++ application/config/config.php
@@ -23,13 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (ENVIRONMENT !== 'production') ? 'http://localhost/e-com/' : '';
+$config['base_url'] = (ENVIRONMENT !== 'production') ? 'http://localhost/E-com/' : '';
 
-
-$config['upload_path'] = './uploads/';
-
-
-$config['upload_path'] = './uploads/';
 
 /*
 |--------------------------------------------------------------------------
